d515c78b8f27019b1e12d2b12ddbbb370390ad1e
Cole Barach <colebarach@tutanota.com>
2026-05-27 13:01:46 -0400

    Updated reference MATLAB TV implementation.

--- doc/controls/tv_bicycle_model_tucker.m
+++ doc/controls/tv_bicycle_model_tucker.m
@@ -1,7 +1,8 @@
 N = 101
+M = 101
 
-#x = linspace (-11, 11, N)
-x = linspace (0, 500, N)
+x = linspace (-600, 600, N);
+y = linspace (-11, 11, M);
 
 torqueRl = []
 torqueRr = []
@@ -9,6 +10,7 @@ torqueFl = []
 torqueFr = []
 
 for i = [1:N]
+  for j = [1:M]
 
   #TODO(Barach): Validate all these
 
@@ -21,8 +23,7 @@ for i = [1:N]
 
   wheelBase = 1.528;
 
-  # wheelAngle = 11 * x (i) / 500;
-  wheelAngle = 11;
+  wheelAngle = y (j);
 
   lx = trackWidthFront / 2.0;
   ly = wheelBase * frontRearWeightBias;
@@ -35,10 +36,13 @@ for i = [1:N]
   yawTransferFl =  gearRatio * lr / wheelRadius * cosd (a - wheelAngle);
   yawTransferFr = -gearRatio * lr / wheelRadius * cosd (a + wheelAngle);
 
-  throttlePosition = 0.5;
+  throttlePosition = 0.0;
   torqueLimit = 84 * throttlePosition;
 
   momentIdeal = x (i);
+
+  drivingTorqueLimit = 21;
+  regenTorqueLimit = -21;
 
   # Option 1 (Will likely overload the FL tire in understeer conditions)
 
@@ -72,52 +76,104 @@ for i = [1:N]
 
   # Option 3 (Doesn't respect driving F/R bias in oversteer/understeer conditions)
 
+##  drivingBiasFr = 0.6;
+##
+##  A = [1,             1,             1,             1,
+##       yawTransferRl, yawTransferRr, yawTransferFl, yawTransferFr,
+##       0,             2,             0,             0,
+##       0,             0,             2,             0]
+##
+##  B = [torqueLimit,
+##       momentIdeal,
+##       drivingBiasFr * torqueLimit,
+##       (1-drivingBiasFr) * torqueLimit]
+##
+##  C = A^-1 * B;
+##
+##  torqueRl (i) = C (1);
+##  torqueRr (i) = C (2);
+##  torqueFl (i) = C (3);
+##  torqueFr (i) = C (4);
+
+  # Current Implementation
+
   drivingBiasFr = 0.6;
 
-  A = [1,             1,             1,             1,
-       yawTransferRl, yawTransferRr, yawTransferFl, yawTransferFr,
-       0,             2,             0,             0,
-       0,             0,             2,             0]
+  if (wheelAngle >= 0)
+  #if (momentIdeal >= 0)
+    yawTransferRo = yawTransferRl;
+    yawTransferRi = yawTransferRr;
+    yawTransferFo = yawTransferFl;
+    yawTransferFi = yawTransferFr;
+  else
+    yawTransferRo = yawTransferRr;
+    yawTransferRi = yawTransferRl;
+    yawTransferFo = yawTransferFr;
+    yawTransferFi = yawTransferFl;
+  endif
 
-  B = [torqueLimit,
-       momentIdeal,
-       drivingBiasFr * torqueLimit,
-       (1-drivingBiasFr) * torqueLimit]
+  torqueRi = drivingBiasFr * torqueLimit / 2;
+  torqueFo = (1 - drivingBiasFr) * torqueLimit / 2;
 
-  C = A^-1 * B;
+  momentRequired = momentIdeal - torqueRi * yawTransferRi - torqueFo * yawTransferFo;
 
-  torqueRl (i) = C (1);
-  torqueRr (i) = C (2);
-  torqueFl (i) = C (3);
-  torqueFr (i) = C (4);
+  torqueRo =  (momentRequired - 0.5 * torqueLimit * yawTransferFi) / (yawTransferRo - yawTransferFi);
+  torqueFi = -(momentRequired - 0.5 * torqueLimit * yawTransferRo) / (yawTransferRo - yawTransferFi);
 
-  torqueRr2 (i) = drivingBiasFr * torqueLimit / 2;
-  torqueFl2 (i) = (1 - drivingBiasFr) * torqueLimit / 2;
+  # TODO(Barach): Does not work.
 
-  momentRequired = momentIdeal - torqueRr2 (i) * yawTransferRr - torqueFl2 (i) * yawTransferFl
+  if (torqueRo > drivingTorqueLimit)
+    torqueRo = drivingTorqueLimit
+    torqueFi = torqueLimit/2 - drivingTorqueLimit
+  endif
+  if (torqueFi > drivingTorqueLimit)
+    torqueFi = drivingTorqueLimit
+    torqueRo = torqueLimit/2 - drivingTorqueLimit
+  endif
 
-  torqueRl2 (i) =  (momentRequired - 0.5 * torqueLimit * yawTransferFr) / (yawTransferRl - yawTransferFr);
-  torqueFr2 (i) = -(momentRequired - 0.5 * torqueLimit * yawTransferRl) / (yawTransferRl - yawTransferFr);
+  if (torqueRo < regenTorqueLimit)
+    torqueRo = regenTorqueLimit;
+    torqueFi = torqueLimit/2 - regenTorqueLimit;
+  endif
+  if (torqueFi < regenTorqueLimit)
+    torqueFi = regenTorqueLimit;
+    torqueRo = torqueLimit/2 - regenTorqueLimit;
+  endif
 
+  if (wheelAngle >= 0)
+  #if (momentIdeal >= 0)
+    torqueRl (j,i) = torqueRo;
+    torqueRr (j,i) = torqueRi;
+    torqueFl (j,i) = torqueFo;
+    torqueFr (j,i) = torqueFi;
+  else
+    torqueRl (j,i) = torqueRi;
+    torqueRr (j,i) = torqueRo;
+    torqueFl (j,i) = torqueFi;
+    torqueFr (j,i) = torqueFo;
+  endif
+
+endfor
 endfor
 
 figure;
 hold on;
-plot (x, torqueRl);
-plot (x, torqueRl2);
 
-#figure;
-#hold on;
+#surf (x, y (51:101), torqueRl (51:101,:));
+#surf (x, y (1:49),   torqueRr (1:49,:));
+#surf (x, y (1:49),   torqueFl (1:49,:));
+#surf (x, y (51:101), torqueFr (51:101,:));
 
-#plot (x, torqueRl, c='r');
-#plot (x, torqueRr, c='b');
-#plot (x, torqueFl, c='y');
-#plot (x, torqueFr, c='g');
+surf (x, y, torqueFl);
+surf (x, y, torqueFr);
 
-#plot (x, zeros (N))
-#plot (x, 21 * ones (N))
+#plot (x, drivingTorqueLimit * ones (N));
+#plot (x, regenTorqueLimit * ones (N));
 
-legend ("RL", "RR", "FL", "FR");
+#plot ([momentMax, momentMax], [regenTorqueLimit, drivingTorqueLimit])
+#plot ([momentMin, momentMin], [regenTorqueLimit, drivingTorqueLimit])
 
-xlabel ("Moment Request (Nm)");
-ylabel ("Motor Torques (Nm)");
+#legend ("RL", "RR", "FL", "FR");
+
+#xlabel ("Moment Request (Nm)");
+#ylabel ("Motor Torques (Nm)");
